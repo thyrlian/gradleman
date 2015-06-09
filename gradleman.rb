64a97b29d7d1998c24a50937debbd2dc5278bd2b
Jing Li <jingli@ebay.com>
2015-06-10 00:12:00 +0200

    Gradleman borns here and now!

---
+++ gradleman.rb
@@ -0,0 +1,101 @@
+#!/usr/bin/env ruby
+
+module Gradleman
+  class MultiDexInspector
+    class << self
+      def list_dependency_hierarchy(path, project)
+        cmd = "cd #{path} && gradle :#{project}:dependencies"
+        begin
+          output = `#{cmd}`
+        rescue Exception => e
+          puts e.message
+        end
+        output
+      end
+      
+      def parse(output)
+        regex_dep = /(((\w+\.)*+\w+:[\w\-\.]+):(.*+))|(:(.*?):)/
+        regex_cfg = /^([^\s]*)?\s-\s.*/
+        set = {}
+        cfg = nil
+        output.lines do |l|
+          if regex_cfg.match(l)
+            cfg = $~[1]
+            set[cfg] = []
+          elsif regex_dep.match(l) && !cfg.nil?
+            if $~[6].nil?
+              name = $~[2]
+              version = $~[4]
+            else
+              name = $~[6]
+              version = nil
+            end
+            collision = false
+            set[cfg].each do |x|
+              if x[:name] == name
+                collision = true
+                x[:versions].push(version)
+                break
+              end
+            end
+            unless collision
+              set[cfg].push({:name => name, :versions => [version]})
+            end
+          end
+        end
+        return set
+      end
+      
+      def inspect(set)
+        results = {}
+        set.each do |k, v|
+          results[k] = []
+          v.each do |x|
+            if x[:versions].size > 1
+              results[k].push(x)
+            end
+          end
+        end
+        return results
+      end
+      
+      def report(results)
+        results.each do |k, v|
+          puts k
+          if v.size > 0
+            puts StringUtil.colorize('Collision found:', :red)
+            v.each do |x|
+              puts StringUtil.colorize(x[:name], :yellow) + " => #{x[:versions]}"
+            end
+          else
+            puts StringUtil.colorize('No collision', :green)
+          end
+          puts "\n" + '=' * 70 + "\n\n"
+        end
+      end
+      
+      def analyze(path, project)
+        report(inspect(parse(list_dependency_hierarchy(path, project))))
+      end
+    end
+    
+    private_class_method :list_dependency_hierarchy, :parse, :inspect, :report
+  end
+  
+  class StringUtil
+    class << self
+      def colorize(text, color)
+        case color.intern
+        when :red
+          "\e[31m#{text}\e[0m"
+        when :green
+          "\e[32m#{text}\e[0m"
+        when :yellow
+          "\e[33m#{text}\e[0m"
+        else
+          text
+        end
+      end
+    end
+  end
+end
